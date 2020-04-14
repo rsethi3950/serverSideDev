2799853b1383d3f788605b96f5e68395f938e95a
Riya Sethi <sethi3950@gmail.com>
2020-04-14 21:41:14 +0530

    updated

--- routes/dishrouter.js
+++ routes/dishrouter.js
@@ -4,22 +4,22 @@ const bodyParser = require('body-parser');
 const dishRouter = express.Router();
 dishRouter.use(bodyParser.json());
 dishRouter.route('/')
-dishRouter.all('/dishes', function (req, res, next) {
+.all( function (req, res, next) {
     res.statusCode = 200;
     console.log('all');
     res.setHeader = ('Content-type', 'text/plain');
     next();
 })
 
-dishRouter.get('/dishes', function (req, res, next) {
+.get( function (req, res, next) {
     res.end('will send dishes to you!');
 })
 
-dishRouter.post('/dishes', function (req, res, next) {
+.post(function (req, res, next) {
     res.end('will add dish' + req.body.name + 'with details : ' + req.body.description);
 })
 
-dishRouter.put('/dishes', function (req, res, next) {
+.put( function (req, res, next) {
     res.statusCode = 403;
     res.end('PUT cannot be performed on /dishes');
 });
